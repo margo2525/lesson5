3140708edee5ffe1a34d94cb77dea73921684925
Svetlana <svetlanalasiichyk@gmail.com>
2022-10-30 00:19:01 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,85 @@
+
+//Створіть дві змінні a та b. Виведіть у консоль результат їх множення.
+//const a = 3;
+//const b = 2;
+//t = a * b;
+//console.log('t :>> ', t);
+// Створіть дві змінні c та d. Виведіть у консоль результат ділення першого на друге.
+//const c = 10;
+//const d = 2;
+//w = c / d;
+//console.log('w :>> ', w);
+
+
+// Створіть дві змінні e та f. Виведіть у консоль результат додавання.
+//const e = 1;
+//const f = 2;
+//k = e + f;
+//console.log('k :>> ', k);
+
+// Визначте змінні зі значеннями 11, true, "java script", "100" і виведіть їх в консоль
+//let Number = 11;
+//let g = true;
+//let Name = "java script";
+//let nut = "100";
+
+//console.log('Number :>> ', Number);
+//console.log('g :>> ', g);
+//console.log('Name :>> ', Name);
+//console.log('nut :>> ', nut);
+
+
+// Переробіть наведений код так, щоб у ньому використовувалися операції +=, -=, *=, /=, ++, --
+//num = num + 11;
+//let num = 1;
+//num += 11;
+//console.log('num :>> ', num);
+//num = num – 11;
+let num = 1;
+num -= 11;
+console.log('num :>> ', num);
+//num = num * 11;
+//let num = 1;
+//num *= 11;
+//console.log('num :>> ', num);
+//num = num / 11;
+//let num = 1;
+//num /= 11;
+//.log('num :>> ', num);
+//num = num + 1;
+//let num = 1;
+//num += 1;
+//console.log('num :>> ', num);
+//num = num – 1;
+//let num = 1;
+//num -= 1;
+//console.log('num :>> ', num);
+//Таски на prompt (вводити з допомогою prompt, виводити з допомогою alert):
+
+//1) Запитати число у користувача, піднести його до квадрату і вивести результат.
+//const inputValue = Number(prompt('Введіть число:'));
+//const alert = inputValue ** 2;
+//console.log('alert :>> ', alert);
+
+
+
+
+//2) Вивести середнє арифметичне двох чисел. Числа запитувати у користувача.
+//const a = +prompt('input value');
+//const b = Number(prompt('input value'));
+//const result = (a + b) / 2;
+//console.log('You summ is ', result);
+
+
+//3) Перевести кількість хвилин на секунди та вивести результат. Число хвилин запитувати у користувача.
+//const a = Number(prompt('input value'));
+//const result = a * 60;
+//console.log('You summ is ', result);
+
+
+
+//4) Створіть змінну greeting зі значенням 'Hello,' і оголосіть змінну userName . Запитайте ім'я користувача та внесіть це значення у змінну userName. Виведіть повідомлення з привітанням, наприклад, 'Hello, Vasya!'. (згадайте про конкатенацію)
+let userName = (prompt('input value'));
+const greeting = "Helo + userName )!";
+console.log('greeting :>> ', greeting);
+//не получается в консоли выводит   greeting :>>  Helo + userName )!
